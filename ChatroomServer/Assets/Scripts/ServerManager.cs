4cbcd57744268ae2aeedd1b8e226c57093840e22
nirupamkumar <mundra.nirupamkumar@gmail.com>
2023-06-05 00:19:57 +0530

    Update ServerManager.cs

--- ChatroomServer/Assets/Scripts/ServerManager.cs
+++ ChatroomServer/Assets/Scripts/ServerManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using WebSocketSharp.Server;
 
 public class ServerManager : MonoBehaviour
 {
     private WebSocketServer _server;
+
+    public TMP_InputField inputField;
 
     private void Start()
     {
@@ -14,8 +17,24 @@ public class ServerManager : MonoBehaviour
         _server.Start();
     }
 
+    private void Update()
+    {
+        OnApplicationQuit();
+    }
+
     private void OnApplicationQuit()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(StopServer());
+            Application.Quit();
+        }
+        
+    }
+
+    IEnumerator StopServer()
+    {
+        yield return new WaitForSecondsRealtime(3);
         _server.Stop();
     }
 }
