5df3f333f89c03be988f949482d0908053bba42d
nirupamkumar <mundra.nirupamkumar@gmail.com>
2023-06-05 02:52:30 +0530

    Create ChatServerBehaviour.cs

---
+++ ChatroomServer/Assets/Scripts/ChatServerBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class ChatServerBehaviour : WebSocketBehavior
+{
+    //private static List<string> _charRooms = new List<string>();
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Sessions.Broadcast(e.Data);
+        string message = e.Data;
+    }
+
+    protected override void OnOpen()
+
+    {
+        //base.OnOpen();
+    }
+
+    protected override void OnClose(CloseEventArgs e)
+    {
+        //base.OnClose(e);
+    }
+}
