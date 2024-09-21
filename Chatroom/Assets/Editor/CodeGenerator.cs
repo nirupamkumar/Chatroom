1c4c5ea965e5743e7238a12f5129a53779ef0686
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-09-21 23:39:21 +0200

    Update CodeGenerator.cs

--- Chatroom/Assets/Editor/CodeGenerator.cs
+++ Chatroom/Assets/Editor/CodeGenerator.cs
@@ -6,6 +6,7 @@ using System.IO;
 using Unity.VisualScripting;
 using Mono.Cecil;
 using System.Reflection;
+using System;
 
 public class CodeGenerator : MonoBehaviour
 { 
@@ -14,7 +15,7 @@ public class CodeGenerator : MonoBehaviour
     {
         MonoScript classToSerialize = (MonoScript)Resources.LoadAll("ClassToDeSerialize")[0];
 
-        using (FileStream stream = new FileStream ($"{Application.dataPath}/Scripts/DemoSerializer.cs", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        using (FileStream stream = new FileStream ($"{Application.dataPath}/Scripts/Serializer.cs", FileMode.OpenOrCreate, FileAccess.ReadWrite))
         {
             using (StreamWriter writer = new StreamWriter(stream))
             {
@@ -24,25 +25,51 @@ public class CodeGenerator : MonoBehaviour
                 writer.WriteLine("using UnityEngine;");
                 writer.WriteLine();
                 writer.WriteLine();
-                writer.WriteLine("public class DemoSerializer");
+                writer.WriteLine("public class Serializer");
                 writer.WriteLine("{");
-                writer.WriteLine("\tpublic void Display()");
+                writer.WriteLine("\tpublic void Serialize(object obj, BinaryWriter writer)");
                 writer.WriteLine("\t{");
 
-                foreach (FieldInfo fi in classToSerialize.GetClass().GetFields())
-                {
-                    writer.WriteLine($"\t\tDebug.Log(\"{fi.Name}\");");
-                }
+                SerializeFields(classToSerialize.GetClass(), writer);
 
                 writer.WriteLine("\t}");
+                writer.WriteLine();
+                writer.WriteLine("\tpublic object Deserializer(BinaryReader reader)");
+                writer.WriteLine("\t{");
+                writer.WriteLine("\t\tobject obj = new object();");
+                writer.WriteLine();
+                writer.WriteLine("\t\tDeserializeFields(ref obj, reader);");
+                writer.WriteLine("\t\treturn obj;");
+                writer.WriteLine("\t}");
+
+                writer.WriteLine();
+                writer.WriteLine("\tprivate void SerializeFields(object obj, BinaryWriter writer)");
+                writer.WriteLine("\t{");
+                //SerializeFieldsRecursive(classToSerialize.GetClass(), "obj", writer);
+                writer.WriteLine("\t}");
+
+                writer.WriteLine();
+                writer.WriteLine("\tprivate void DeserializeFields(ref object obj, BinaryReader reader)");
+                writer.WriteLine("\t{");
+                //DeserializeFieldsRecursive(classToSerialize.GetClass(), "ref obj", writer);
+                writer.WriteLine("\t}");
+
                 writer.WriteLine("}");
-
-
                 AssetDatabase.Refresh();
             }
         }
 
     }
-    
-   
+
+    private static void SerializeFields(Type type, StreamWriter streamWriter)
+    {
+        foreach (var field in type.GetFields())
+        {
+            string fieldName = field.Name;
+            string fieldType = field.FieldType.FullName;
+            streamWriter.WriteLine($"\t\twriter.Write(({fieldType})obj.{fieldName});");
+        }
+    }
+
+
 }
