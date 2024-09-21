48e50f551c1ef93890d72a9f0c62f849f765791d
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-09-21 23:39:43 +0200

    Create ArrayPrimitvetypes.cs

---
+++ Chatroom/Assets/Resources/ClassToDeSerialize/ArrayPrimitiveTypes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class ArrayPrimitiveTypes
+{
+    public int[] intVariables;
+    public float[] floatVariables;
+    public char[] charVariables;
+    public string[] stringVariables;
+    public bool[] boolVariables;
+    public double[] doubleVariables;
+    public short[] shortVariables;
+    public long[] longVariables;
+    public ulong[] ulongVariables;
+    public ushort[] ushortVariables;
+    public uint[] uintVariables;
+
+}
