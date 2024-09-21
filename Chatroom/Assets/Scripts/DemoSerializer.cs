51aba09b69891388f75490f5cbf7c13bc1abf30b
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-09-21 23:40:26 +0200

    Update DemoSerializer.cs

--- Chatroom/Assets/Scripts/DemoSerializer.cs
+++ Chatroom/Assets/Scripts/DemoSerializer.cs
@@ -1,1 +1,14 @@
+using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
+
+
+public class DemoSerializer
+{
+	public void Display()
+	{
+		Debug.Log("health");
+		Debug.Log("mana");
+		Debug.Log("leftTheChat");
+	}
+}
