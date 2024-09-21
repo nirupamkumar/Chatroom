57499f519db3464d82d2668d3412e292cc36df33
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-09-21 23:40:07 +0200

    Create PrimitiveTypes.cs

---
+++ Chatroom/Assets/Resources/ClassToDeSerialize/PrimitiveTypes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrimitiveTypes
+{
+    public int intVariable;
+    public float floatVariable;
+    public char charVariable;
+    public string stringVariable;
+    public bool boolVariable;
+    public double doubleVariable;
+    public short shortVariable;
+    public long longVariable;
+    public ulong ulongVariable;
+    public ushort ushortVariable;
+    public uint uintVariable;
+
+}
