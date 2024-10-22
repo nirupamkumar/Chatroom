d17bbcb8189f365bbcda0c140ecca1501a7f227c
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-10-22 20:16:20 +0200

    [Server] Update Server.cs

--- ChatroomServer/Assets/Scripts/Server.cs
+++ ChatroomServer/Assets/Scripts/Server.cs
@@ -1,62 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using WebSocketSharp.Server;
 
 public class Server : MonoBehaviour
 {
-    private WebSocketServer _server;
-
-    public TMP_Text displayMessages;
-    private string serverName = "[Server]: ";
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        try
-        {
-            _server = new WebSocketServer(15001);
-            _server.AddWebSocketService<ChatServerBehaviour>("/chat");
-            _server.Start();
-
-            displayMessages.text = string.Empty;      
-            displayMessages.text = string.Join("\n", serverName + "Server Started" + "\n");
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Failed to start server: {ex.Message}");
-        }
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            StopServer();
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        StopServer();
-    }
-
-    private void StopServer()
-    {
-        if (_server != null)
-        {
-            _server.Stop();
-            _server = null;
-            Debug.Log("Server stopped");
-
-            string leaveMessage = "Server stopped";
-            displayMessages.text = serverName + leaveMessage;
-        }
-    }
-
-    public void QuitApplicationWindow()
-    {
-        Application.Quit();
+        
     }
 }
